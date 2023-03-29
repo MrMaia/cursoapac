2c34ee7855878a53c4427145bdea5e93c5c0337a
Allan Maia <allanmaiasantos51@hotmail.com>
2023-03-29 09:49:37 -0300

    update

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+$login = $_POST['user_id'];
+$entrar = $_POST['entrar'];
+$senha = md5($_POST['user_pass']);
+$connect = mysql_connect('localhost','','');
+$db = mysql_select_db('autoapac');
+  if (isset($entrar)) {
+
+    $verifica = mysql_query("SELECT * FROM usuarios WHERE login =
+    '$login' AND senha = '$senha'") or die("erro ao selecionar");
+      if (mysql_num_rows($verifica)<=0){
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Login e/ou senha incorretos');window.location
+        .href='login.html';</script>";
+        die();
+      }else{
+        setcookie("login",$login);
+        header("Location:index.php");
+      }
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -15,7 +37,7 @@
             <input class="form_input" type="text" placeholder="Insira seu ID" id="user_id" required>
             <label class="form_label" for="user_pass"></label>
             <input class="form_input" type="password" placeholder="Insira sua Senha" id="user_pass" required>
-            <button type="submit" class="button">Entrar</button>
+            <button type="submit" class="button" id="entrar">Entrar</button>
             <img class="form_img" src="assets/Logo apac.png" alt="Logo Apac">
         </form>
     </div>
